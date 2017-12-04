7d9daf6448b2f4ffaf60a90160a313531b121c59
josephgarrido <josephpgarrido@gmail.com>
2017-12-04 13:26:54 -0800

    changes

--- deployment.bash
+++ deployment.bash
@@ -1,15 +1,21 @@
 #!/bin/bash
-
+echo Building site with drush make
+ 
 drush make mymake.make jpg
 
+# message
+echo change directory into root and run drush site install minimal. 
+ 
 cd jpg
 
+# message
 echo deploying minimal site... 
 
 drush si minimal --site-name=jpgarrido --db-url=mysql://root:root@localhost/drupal-make -y
 
 drush upwd admin --password="welcome1" -y
 
-drush vset theme_default basic 
+# Lets move this to the install profile
+# drush vset theme_default basic 
 
 drush cc all -y
