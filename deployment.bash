78f4eab9331fec5ab5cd1e8105506c3403c2605b
josephgarrido <josephpgarrido@gmail.com>
2017-12-05 13:31:42 -0800

    functioning deployment script

--- deployment.bash
+++ deployment.bash
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+buildithere="/Users/josephgarrido/Documents/sites/jpg"
+script="/Users/josephgarrido/Documents/personal-git/drupal-deployment-script/site_template"
+
 say building your site
-cd deploy-site
- 
-drush make drupal-org.make /Users/josephgarrido/Documents/sites/jpg
+
+drush make $script/site_template.make $buildithere
+
+cp -R $script $buildithere/profiles
+cd $buildithere
 
 drush si site_template --site-name=bootstrap --db-url=mysql://root:root@localhost/drupal-make -y
+drush upwd admin --password="welcome1"
 
+# drush sql-drop
+# drush sql-cli < ~ existing db w content.sql
