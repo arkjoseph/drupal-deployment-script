2c57c17a75644287510436cfe147d0ad6d43de7f
josephgarrido <josephpgarrido@gmail.com>
2017-12-08 12:16:35 -0800

    new branch and server settings

--- deployment.bash
+++ deployment.bash
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-buildithere="/Users/josephgarrido/Documents/sites/jpg"
-script="/Users/josephgarrido/Documents/personal-git/drupal-deployment-script/site_template"
+buildithere="/home/jpgio387/public_html/development"
+script="site_template"
 dbname="jpg-12-6-17.sql"
 
-say building your site
+#say building your site
 
 drush make $script/site_template.make $buildithere
 
 cp -R $script $buildithere/profiles
 cd $buildithere
 
-drush si site_template --site-name=bootstrap --db-url=mysql://root:root@localhost/drupal-make -y
+drush si site_template --site-name=bootstrap --db-url=mysql://jpgio387:Athlon101@localhost/jpgio387_1a -y
 drush upwd admin --password="welcome1"
 
 cd $buildithere/profiles/site_template/assets
 drush sql-drop -y
 drush sql-cli < $dbname
-say build complete
+#say build complete
 
-drush user-login --uri=http://jpg --redirect-port=8888 -y
-
-
-drush si minimal --site-name=bootstrap --db-url=mysql://jpgio387_1a:Athlon101@localhost/jpgio387_1a -y
-
-#drush rsync --exclude-conf @jpgio.local @jpgio.dev
-#drush @jpgio.dev fua -y
-#drush @jpgio.dev fra -y
+#drush user-login --uri=http://jpg --redirect-port=8888 -y
+#drush make /home/jpgio387/public_html/drupal-deployment-script/site_template/site_template.make /home/jpgio387/public_html/development
+#drush si minimal --site-name=bootstrap --db-url=mysql://jpgio387_1a:Athlon101@localhost/jpgio387_1a -y
