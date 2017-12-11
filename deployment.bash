55d858fae7209b695f348536b677076bfb81cdb0
Joseph Local <josephpgarrido@gmail.com>
2017-12-11 01:27:33 -0800

    drush alias

--- deployment.bash
+++ deployment.bash
@@ -11,7 +11,7 @@ drush make $script/site_template.make $buildithere
 cp -R $script $buildithere/profiles
 cd $buildithere
 
-drush si site_template --site-name=bootstrap --db-url=mysql://jpgio387:Athlon101@localhost/jpgio387_1a -y
+drush si site_template --site-name=bootstrap --db-url=mysql://jpgio387_develop:Athlon101@localhost/jpgio387_develop -y
 
 cd $buildithere/profiles/site_template/assets
 drush sql-drop -y
@@ -26,3 +26,13 @@ drush upwd admin --password="welcome1"
 #drush user-login --uri=http://jpg --redirect-port=8888 -y
 #drush make /home/jpgio387/public_html/drupal-deployment-script/site_template/site_template.make /home/jpgio387/public_html/development
 #drush si minimal --site-name=bootstrap --db-url=mysql://jpgio387_1a:Athlon101@localhost/jpgio387_1a -y
+
+## Remote Drush Alias
+# drush @jpg.dev si site_template --site-name=bootstrap --db-url=mysql://jpgio387_develop:Athlon101@localhost/jpgio387_develop -y
+
+
+
+
+
+
+
